fddf943fa0ce22b7965cdfffa6406bf9978ee9e3
jeovane16 <jeovanearaujo16@gmail.com>
2020-10-12 18:04:19 -0300

    Criação da pagina incial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { FiArrowRight } from "react-icons/fi";
 import "./styles/global.css";
+import "./styles/pages/landing.css";
+
+import logoImg from "./assets/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Logo da Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Curitiba</strong>
+          <span>Paraná</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
     </div>
   );
 }
