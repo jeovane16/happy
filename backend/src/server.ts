5912c59780f3db88925111070e3bd0432fa6deae
jeovane16 <jeovanearaujo16@gmail.com>
2020-10-13 19:08:18 -0300

    Conexão com o banco de dados

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import "./database/connection";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/users", (request, response) => {
+  response.json({ message: "hello world" });
+});
+app.listen(3333);
